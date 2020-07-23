f093398a4fb51c2f5da3bce51b2bb1b5cbdf8cf3
Daria <dariaalbufeira@gmail.com>
2020-07-23 11:03:24 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Attendance_Performance_Control.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
