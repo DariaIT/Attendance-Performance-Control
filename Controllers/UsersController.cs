84466a8535c936cc81ace09d70222c20850635ee
Daria <dariaalbufeira@gmail.com>
2020-08-05 10:58:59 +0100

    Create-Confirm-User-By-Admin-Page

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -37,5 +37,42 @@ namespace Attendance_Performance_Control.Controllers
             var users = await _userManager.Users.Where(c=>c.Id != userAdminId).ToListAsync();
             return View(users);
         }
+
+
+        //@confirm - false - show page confirmation
+        //@confirm - true - confirm user's email
+        [HttpPost]
+        public async Task<IActionResult> Confirm(string Id, string confirm)
+        {
+            var user = _userManager.FindByIdAsync(Id).Result;
+            if (user != null)
+            {
+                //if @confirm - true - confirm user's email
+                if (String.Compare(confirm,"1")==0)
+                {
+                    try
+                    {
+                        user.EmailConfirmed = true;
+                        await _context.SaveChangesAsync();
+                        ModelState.AddModelError("confirmsuccess", "O registo de utilizador está confirmado e email de confirmação está enviado.");
+                        return RedirectToAction(nameof(Index));
+                    }
+                    catch (DbUpdateException)
+                    {
+                        ModelState.AddModelError(String.Empty, "Não foi possível salvar as alterações." +
+                                                               "Tente novamente e se o problema persistir," +
+                                                               "notifique o administrador do sistema.");
+                    }
+                    
+                }
+                return View(user);
+            }
+            else
+            {
+                ModelState.AddModelError(String.Empty, "Utilizador não existe");
+            }
+            return View();
+        }
+
     }
 }
